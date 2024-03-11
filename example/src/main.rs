8014e250a92a41c35299a176314a3c78ea5c8d67
PotatoMaaan <60669730+PotatoMaaan@users.noreply.github.com>
2024-03-12 00:46:47 +0100

    minimal example works

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,7 +1,48 @@
-use std::{net::SocketAddrV4, str::FromStr};
+use std::{
+    net::SocketAddrV4,
+    str::FromStr,
+    sync::{atomic::AtomicU64, Arc},
+};
+
+use mttp::{HttpRequest, HttpResponse};
+
+struct State {
+    counter: AtomicU64,
+}
 
 fn main() {
-    mttp::start_server(std::net::SocketAddr::V4(
+    let mut server = mttp::Server::new(State {
+        counter: AtomicU64::new(0),
+    });
+
+    server.get("/hello", hello);
+    server.post("/echo", echo);
+
+    server.start(std::net::SocketAddr::V4(
         SocketAddrV4::from_str("127.0.0.1:5000").unwrap(),
     ));
+}
+
+fn hello(state: Arc<State>, req: HttpRequest) -> HttpResponse {
+    let new_count = state
+        .counter
+        .fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+
+    println!("HANDLER HELLO");
+
+    HttpResponse {
+        status: 200,
+        msg: "Ok".to_owned(),
+        body: Some(format!("Hello {}", new_count).as_bytes().to_vec()),
+    }
+}
+
+fn echo(state: Arc<State>, req: HttpRequest) -> HttpResponse {
+    println!("HANDLER ECHO");
+
+    HttpResponse {
+        status: 200,
+        msg: "Ok".to_owned(),
+        body: req.body,
+    }
 }
--- mttp/src/lib.rs
+++ mttp/src/lib.rs
@@ -2,6 +2,7 @@ use std::{
     collections::HashMap,
     io::{self, BufRead, Read, Write},
     net::{SocketAddr, TcpListener, TcpStream},
+    sync::Arc,
     time::Duration,
 };
 
@@ -27,33 +28,81 @@ impl From<io::Error> for Error {
     }
 }
 
-pub fn start_server(addr: SocketAddr) {
-    let socket = TcpListener::bind(addr).expect("Failed to start server");
+#[derive(Debug)]
+pub struct Server<State: 'static + Send + Sync> {
+    state: Arc<State>,
+    get_handlers: HashMap<String, fn(Arc<State>, HttpRequest) -> HttpResponse>,
+    post_handlers: HashMap<String, fn(Arc<State>, HttpRequest) -> HttpResponse>,
+}
 
-    println!("Listening...");
-    while let Ok((mut stream, addr)) = socket.accept() {
-        stream
-            .set_read_timeout(Some(Duration::from_secs(5)))
-            .unwrap();
-        println!("New connection from: {:?}", &addr);
-        std::thread::spawn(move || {
-            let req = handle_http(&mut stream);
-            dbg!(&req);
-
-            let req = match req {
-                Ok(v) => v,
-                Err(e) => {
-                    stream
-                        .write_all(b"HTTP/1.1 400 Bad Request\r\n")
-                        .expect("Failed to write to stream");
-                    return;
-                }
-            };
-
-            dbg!(&req);
-        });
+impl<State: 'static + Send + Sync> Server<State> {
+    pub fn new(state: State) -> Self {
+        Self {
+            get_handlers: HashMap::new(),
+            post_handlers: HashMap::new(),
+            state: Arc::new(state),
+        }
     }
-    println!("Server exited");
+
+    pub fn get(&mut self, route: &str, handler: fn(Arc<State>, HttpRequest) -> HttpResponse) {
+        self.get_handlers.insert(route.to_owned(), handler);
+    }
+
+    pub fn post(&mut self, route: &str, handler: fn(Arc<State>, HttpRequest) -> HttpResponse) {
+        self.post_handlers.insert(route.to_owned(), handler);
+    }
+
+    pub fn start(self, addr: SocketAddr) {
+        let socket = TcpListener::bind(addr).expect("Failed to start server");
+
+        println!("Listening...");
+        while let Ok((mut stream, addr)) = socket.accept() {
+            // stream
+            //     .set_read_timeout(Some(Duration::from_secs(5)))
+            //     .unwrap();
+            println!("New connection from: {:?}", &addr);
+
+            let get_handlers = self.get_handlers.clone();
+            let post_handlers = self.post_handlers.clone();
+            let state = self.state.clone();
+            std::thread::spawn(move || {
+                let req = handle_http(&mut stream);
+
+                let req = match req {
+                    Ok(v) => v,
+                    Err(e) => {
+                        stream
+                            .write_all(b"HTTP/1.1 400 Bad Request\r\n")
+                            .expect("Failed to write to stream");
+                        println!("ERR: {:?}", e);
+                        return;
+                    }
+                };
+
+                let handler = match req.method {
+                    Method::Get => get_handlers.get(&req.uri),
+                    Method::Post => post_handlers.get(&req.uri),
+                    _ => None,
+                };
+
+                if let Some(handler) = handler {
+                    let res = handler(state, req);
+
+                    stream
+                        .write_all(format!("\r\nHTTP/1.1 {} {}", res.status, res.msg).as_bytes())
+                        .expect("Failed to send res");
+
+                    if let Some(body) = res.body {
+                        stream.write_all(CHUNK_END).expect("Failed to write to res");
+                        stream.write_all(&body).expect("Failed to write to res");
+                    }
+
+                    stream.write_all(CHUNK_END).expect("Failed to write to res");
+                }
+            });
+        }
+        println!("Server exited");
+    }
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -73,6 +122,13 @@ pub struct HttpRequest {
     pub body: Option<Vec<u8>>,
 }
 
+#[derive(Debug)]
+pub struct HttpResponse {
+    pub status: u16,
+    pub msg: String,
+    pub body: Option<Vec<u8>>,
+}
+
 fn handle_http(stream: &mut TcpStream) -> Result<HttpRequest, Error> {
     let header_chunk = read_header(stream)?;
     let mut lines = header_chunk.lines();
@@ -84,6 +140,7 @@ fn handle_http(stream: &mut TcpStream) -> Result<HttpRequest, Error> {
 
     let method = match first_line.next() {
         Some("GET") => Method::Get,
+        Some("POST") => Method::Post,
         wrong_method => {
             return Err(Error::InvalidMethod {
                 recieved: wrong_method.unwrap_or("").to_owned(),
